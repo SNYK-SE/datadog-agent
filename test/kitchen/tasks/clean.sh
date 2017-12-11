7c68484497b904df3a98abfc106eee433bca1cd6
Greg Meyer <greg.meyer@datadoghq.com>
2017-12-11 13:38:06 -0500

    more env vars

--- test/kitchen/tasks/clean.sh
+++ test/kitchen/tasks/clean.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash -l
 
+if [ -z ${AZURE_CLIENT_ID+x} ]; then$(aws ssm get-parameter --region us-east-1 --name ci.dd-agent-testing.azure_client_id --with-decryption --query "Parameter.Value" --out text)
+fi
+if [ -z ${AZURE_CLIENT_SECRET+x} ]; then
+  export AZURE_CLIENT_SECRET=$(aws ssm get-parameter --region us-east-1 --name ci.dd-agent-testing.azure_client_secret --with-decryption --query "Parameter.Value" --out text)
+fi
+if [ -z ${AZURE_TENANT_ID+x} ]; then
+  export AZURE_TENANT_ID=$(aws ssm get-parameter --region us-east-1 --name ci.dd-agent-testing.azure_tenant_id --with-decryption --query "Parameter.Value" --out text)
+fi
+if [ -z ${AZURE_SUBSCRIPTION_ID+x} ]; then
+  export AZURE_SUBSCRIPTION_ID=$(aws ssm get-parameter --region us-east-1 --name ci.dd-agent-testing.azure_subscription_id --with-decryption --query "Parameter.Value" --out text)
+fi
+
+az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+
 vms=($(az vm list --query "[?starts_with(name, 'dd-agent-testing')]|[*].name" --output tsv))
 
 for vm in $vms; do
